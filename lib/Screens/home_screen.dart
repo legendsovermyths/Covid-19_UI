46c07aec1c2fc3cf49413c3f7aee44d11b3f490c
Mask Wolf <maskwolf2000@gmail.com>
2020-08-01 01:31:30 +0530

    info card ready

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter_svg/flutter_svg.dart';
 import "package:covid_19_ui/constants.dart";
+import 'package:covid_19_ui/widgets/lineChart.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,62 +19,85 @@ class HomeScreen extends StatelessWidget {
         ),
         child: Wrap(
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8)
-              ),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          alignment: Alignment.center,
-                          height: 30,
-                          width: 30,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFFF9C00).withOpacity(0.12),
-                            shape: BoxShape.circle,
-                          ),
-                          child: SvgPicture.asset("assets/icons/running.svg",
-                          height: 12,
-                          width: 12,),
-
-                        ),
-                        SizedBox(width: 5),
-                        Text(
-                          "Confirmed Cases",
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        )
-                      ],
-                    ),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      RichText(
-                        text: TextSpan(
-                          style:TextStyle(color: ktextColor),
-                          children: [
-                            TextSpan(
-                                text: "1,062",
-                              style: Theme.of(context).textTheme.headline6.copyWith(fontWeight: FontWeight.bold)
-                            ),
-                            TextSpan(
-                              text:"People"
-                            )
-                          ]
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
+            infoCard(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class infoCard extends StatelessWidget {
+  String title;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8)
+      ),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  height: 30,
+                  width: 30,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFF9C00).withOpacity(0.12),
+                    shape: BoxShape.circle,
+                  ),
+                  child: SvgPicture.asset("assets/icons/running.svg",
+                  height: 12,
+                  width: 12,),
+
+                ),
+                SizedBox(width: 5),
+                Text(
+                  "Confirmed Cases",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                )
+              ],
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RichText(
+                  text: TextSpan(
+                    style:TextStyle(color: ktextColor),
+                    children: [
+                      TextSpan(
+                          text: "1,062\n",
+                        style: Theme.of(context).textTheme.headline6.copyWith(fontWeight: FontWeight.bold)
+                      ),
+                      TextSpan(
+                        text:"People",
+                        style: TextStyle(
+                          fontSize: 12,
+                          height: 2,
+
+                        )
+                      )
+                    ]
+                  ),
+                ),
+              ),
+              Expanded(
+                child: LineReportChart(),
+              ),
+
+            ],
+          )
+        ],
       ),
     );
   }
@@ -94,3 +118,4 @@ AppBar buildAppBar(){
     ],
   );
 }
+
